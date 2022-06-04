3f4b240791de9f77180f09fe52110fdfea2f13c9
amitGanguly19 <agangul1@my.centennialcollege.ca>
2022-06-04 19:52:14 -0400

    Converted server to express

--- Week03/server.ts
+++ Week03/server.ts
@@ -1,44 +1,16 @@
-//requre buid in modules
-import http from 'http'; // ECMASscript Module pattern (MJS)
-import fs from 'fs';
-import mime from 'mime-types';
+import express from 'express';
 
-let lookup = mime.lookup; //alias for mime.lookup
+const app = express(); //creates  a new application
 
 const port = 3000;
 
-const server = http.createServer(function(req, res)
+app.get('/', function(req, res)
 {
-    let path =req.url as string; // alias for req.url
-
-    if (path == "/" || path == "/home") 
-    {
-        path = "/index.html";
-    }
-
-    let mine_type = lookup(path.substring(1)) as string;
-    console.log(`MIME TYPE: ${mine_type}`);
-
-
-   // fs.readFile(` __dirname: ${__dirname}`);
-   fs.readFile(__dirname + path, function(err, data)
-   {
-       if(err)
-       {
-           res.writeHead(404); //status -file not found
-           return res.end("ERROE: 404 - File Not Found"); //output the error message to the page
-       }
-       // no err
-       res.setHeader("X-Content-Type-Options" , "nosniff"); // security guard
-       res.writeHead(200, {'Content-Type': mine_type}); // status all ok
-       return res.end(data); //output the file that was read to the page
-
-   });
-
+  res.send('Hello, World!');
 });
 
- server.listen(port, function()
- {
-     console.log(`server running at port: ${port}`);
-
- });
+// listener - similar to server listen
+app.listen(port, function() 
+{
+  console.log(`Example app listening on port ${port}`)
+});
