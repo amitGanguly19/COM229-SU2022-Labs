85171c73d5c5578262ee4a77ded58ff1b2be0b8c
amitGanguly19 <agangul1@my.centennialcollege.ca>
2022-06-04 17:05:55 -0400

    added typscript support

--- Week03/server.js
+++ Week03/server.js
@@ -1,44 +1,31 @@
-//requre buid in modules
-const http = require('http'); //common Module pattern (CJS)
-const fs = require('fs');
-const mime = require('mime-types');
-
-let lookup = mime.lookup; //alias for mime.lookup
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const mime_types_1 = __importDefault(require("mime-types"));
+let lookup = mime_types_1.default.lookup;
 const port = 3000;
-
-const server = http.createServer(function(req, res)
-{
-    let path =req.url; // alias for req.url
-
-    if (path == "/" || path == "/home") 
-    {
+const server = http_1.default.createServer(function (req, res) {
+    let path = req.url;
+    if (path == "/" || path == "/home") {
         path = "/index.html";
     }
-
     let mine_type = lookup(path.substring(1));
     console.log(`MIME TYPE: ${mine_type}`);
-
-
-   // fs.readFile(` __dirname: ${__dirname}`);
-   fs.readFile(__dirname + path, function(err, data)
-   {
-       if(err)
-       {
-           res.writeHead(404); //status -file not found
-           return res.end("ERROE: 404 - File Not Found"); //output the error message to the page
-       }
-       // no err
-       res.setHeader("X-Content-Type-Options" , "nosniff"); // security guard
-       res.writeHead(200, {'Content-Type': mine_type}); // status all ok
-       return res.end(data); //output the file that was read to the page
-
-   });
-
+    fs_1.default.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            return res.end("ERROE: 404 - File Not Found");
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.writeHead(200, { 'Content-Type': mine_type });
+        return res.end(data);
+    });
 });
-
- server.listen(port, function()
- {
-     console.log(`server running at port: ${port}`);
-
- });
+server.listen(port, function () {
+    console.log(`server running at port: ${port}`);
+});
+//# sourceMappingURL=server.js.map
